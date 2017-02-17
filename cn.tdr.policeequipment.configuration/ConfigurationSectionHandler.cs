266c25f8a5e78bbf1caf9de05e0ae2d5fb156c69
zh.fang <ian.zh.fang@outlook.com>
2017-02-17 18:02:27 +0800

    add configurationsection handle

---
+++ cn.tdr.policeequipment.configuration/ConfigurationSectionHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace cn.tdr.policeequipment.configuration
+{
+    using System.Configuration;
+    using System.Xml;
+
+    public class ConfigurationSectionHandler : IConfigurationSectionHandler
+    {
+        object IConfigurationSectionHandler.Create(object parent, object configContext, XmlNode section)
+        {
+            return section;
+        }
+    }
+}
---
+++ cn.tdr.policeequipment.module/Module.cs
@@ -0,0 +1,30 @@
+﻿namespace cn.tdr.policeequipment.module
+{
+    using Autofac;
+
+    public abstract class Module
+    {
+        //private static readonly string 
+        private static readonly IContainer Container;
+
+        static Module()
+        {
+            var builder = new ContainerBuilder();
+            builder
+                .RegisterType<data.oracle.EfRepository>()
+                .As<interfaces.IRepository>();
+
+            Container = builder.Build();
+        }
+
+        protected interfaces.IRepository Repository { get; private set; }
+
+        protected Module()
+        {
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                Repository = scope.Resolve<interfaces.IRepository>();
+            }
+        }
+    }
+}
