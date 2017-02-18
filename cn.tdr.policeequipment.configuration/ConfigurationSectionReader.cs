a7fb5aa143b4e6560acf6fe9b244b3f943530a13
zh.fang <ian.zh.fang@outlook.com>
2017-02-18 13:40:58 +0800

    add configurationsection reader

---
+++ cn.tdr.policeequipment.configuration/ConfigurationSectionReader.cs
@@ -0,0 +1,18 @@
+﻿namespace cn.tdr.policeequipment.configuration
+{
+    using System;
+    using System.Configuration;
+
+    public abstract class ConfigurationSectionReader : IConfigurationSectionReader
+    {
+        object IConfigurationSectionReader.Read(string sectionGroupNameOrSectionName)
+        {
+            if (string.IsNullOrWhiteSpace(sectionGroupNameOrSectionName))
+            {
+                throw new ArgumentNullException(nameof(sectionGroupNameOrSectionName));
+            }
+
+            return ConfigurationManager.GetSection(sectionGroupNameOrSectionName);
+        }
+    }
+}
---
+++ cn.tdr.policeequipment.configuration/IConfigurationSectionReader.cs
@@ -0,0 +1,15 @@
+﻿namespace cn.tdr.policeequipment.configuration
+{    
+    /// <summary>
+    /// 配置文件 configurationSection 节点读取器
+    /// </summary>
+    public interface  IConfigurationSectionReader
+    {
+        /// <summary>
+        /// 读取指定配置节点
+        /// </summary>
+        /// <param name="sectionGroupNameOrSectionName">configurationSection 节点名称</param>
+        /// <returns></returns>
+        object Read(string sectionGroupNameOrSectionName);
+    }
+}
