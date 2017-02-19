9cf34c07752cb6608377be3fac8f07ee3778fc3c
zh.fang <ian.zh.fang@gmail.com>
2017-02-19 16:03:33 +0800

    add module test

---
+++ cn.tdr.policeequipment.module.test/MyModuleTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace cn.tdr.policeequipment.module.test
+{
+    [TestClass]
+    public class MyModuleTest
+    {
+        [TestMethod]
+        public void Constructor()
+        {
+            var module = new MyModule();
+            Assert.IsNotNull(module.Repository);
+        }
+    }
+}
