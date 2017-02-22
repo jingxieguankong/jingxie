7f5fe968df1121c1066483daae8b7a49c9e3d9a9
zh.fang <ian.zh.fang@outlook.com>
2017-02-22 18:04:49 +0800

    add mapui

---
+++ cn.tdr.policeequipment.web/Controllers/MapController.cs
@@ -0,0 +1,16 @@
+﻿namespace cn.tdr.policeequipment.web.Controllers
+{
+    using System.Web.Mvc;
+
+    // 地图控制器
+    public class MapController : AnonymousController
+    {
+        // GET: Map
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
